68c1a7e8c911030811a2f557819272fa2c38dae0
CodeReveur <codereveur@gmail.com>
2025-08-02 01:34:02 +0200

    chore: re inirialising the project setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-      </head>
+      <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
       <body>
         {children}
       </body>

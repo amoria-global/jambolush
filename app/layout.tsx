b588caf20ab0b6948bd8f60d1427a1bdb83ecfbf
uwitonze8 <pacific.uwitonze112@gmail.com>
2025-08-02 00:47:25 +0200

    ft:added new folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
       </head>
       <body>
         {children}

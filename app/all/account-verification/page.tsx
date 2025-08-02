3f04d6843044074c17541dc456de680f7bb1f0b4
uwitonze8 <pacific.uwitonze112@gmail.com>
2025-08-03 00:59:10 +0200

    ft:adding not found page and account verification page

---
+++ app/all/account-verification/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react';
+import AccountVerificationPage from '../../pages/account-verification';
+
+
+const AppRouterAccountVerificationPage = () => {
+  return <AccountVerificationPage />;
+};
+
+export default AppRouterAccountVerificationPage;
---
+++ app/all/not-found-page/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+import React from 'react';
+import NotFoundPage from '../../pages/not-found-page'; 
+const AppRouterNotFoundPage = () => {
+  return <NotFoundPage />;
+};
+export default AppRouterNotFoundPage;

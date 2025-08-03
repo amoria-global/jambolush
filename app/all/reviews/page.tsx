5739ab53fb50699ebc12709c2ea0a028dca8cfda
uwitonze8 <pacific.uwitonze112@gmail.com>
2025-08-03 02:24:34 +0200

    ft:adding reviews section

---
+++ app/all/reviews/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import React from 'react';
+import Reviews from '../../pages/reviews'; // relative path from `app/all/reviews` to `app/pages/reviews`
+
+const ReviewsPage = () => {
+  return <Reviews />;
+};
+
+export default ReviewsPage;

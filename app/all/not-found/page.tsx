fce46b8648f1d2ed760b976db82fdf4b77814e80
CodeReveur <codereveur@gmail.com>
2025-08-05 22:30:25 +0200

    bug: fix bootstrap by removing lucide-react, resolving not-found page

--- app/all/not-found/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-import React from 'react';
-import NotFoundPage from '../../pages/not-found'; 
-const AppRouterNotFoundPage = () => {
-  return <NotFoundPage />;
-};
-export default AppRouterNotFoundPage;

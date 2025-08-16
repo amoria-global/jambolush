036f4cc839ac44552de337149907bb303c8a11bd
CodeReveur <codereveur@gmail.com>
2025-08-16 02:05:10 +0200

    feat: add new add review form popal up modal

---
+++ app/all/property/[id]/photos/page.tsx
@@ -0,0 +1,11 @@
+// app/pages/AllPhotosPage.tsx
+
+'use client';
+import React from 'react';
+import AllPhotosContent from '@/app/pages/all_photos';
+
+const AllPhotosPage = () => {
+  return <AllPhotosContent />;
+};
+
+export default AllPhotosPage;
